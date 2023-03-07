a7b6b913d31b69f19dbd04d532726bc3877050fd
Michael Venrick <mvenrick@gmail.com>
2023-03-06 19:41:50 -0500

    Fixed cart

--- script.js
+++ script.js
@@ -5,10 +5,8 @@ const chocolatesPage = document.querySelector(".chocolates");
 const cartPage = document.querySelector(".cart");
 const footPage = document.querySelector("#foot");
 
-
 //default to products page by hiding cart page
 cartPage.style.display = "none";
-
 
 //variables for nav elements
 const appliancesNav = document.querySelector("#appliances");
@@ -16,6 +14,7 @@ const homeNav = document.querySelector("#home");
 const chocolatesNav = document.querySelector("#chocolates");
 const cartNav = document.querySelector("#cart");
 const coffeesNav = document.querySelector("#coffees");
+const continueShopping = document.querySelector("#continue-shopping");
 
 //event listeners for nav elements
 appliancesNav.addEventListener("click", function () {
@@ -30,6 +29,15 @@ homeNav.addEventListener("click", function () {
     appliancesPage.style.display = "block";
     chocolatesPage.style.display = "block";
     coffeesPage.style.display = "block";
+    footPage.style.display = "block";
+});
+
+continueShopping.addEventListener("click", function () {
+    cartPage.style.display = "none";
+    appliancesPage.style.display = "block";
+    chocolatesPage.style.display = "block";
+    coffeesPage.style.display = "block";
+    footPage.style.display = "block";
 });
 
 coffeesNav.addEventListener("click", function () {
@@ -37,6 +45,7 @@ coffeesNav.addEventListener("click", function () {
     chocolatesPage.style.display = "none";
     cartPage.style.display = "none";
     coffeesPage.style.display = "block";
+    footPage.style.display = "block";
 });
 
 chocolatesNav.addEventListener("click", function () {
@@ -44,6 +53,7 @@ chocolatesNav.addEventListener("click", function () {
     coffeesPage.style.display = "none";
     cartPage.style.display = "none";
     chocolatesPage.style.display = "block";
+    footPage.style.display = "block";
 });
 
 cartNav.addEventListener("click", function () {
@@ -52,14 +62,14 @@ cartNav.addEventListener("click", function () {
     chocolatesPage.style.display = "none";
     footPage.style.display = "none";
     cartPage.style.display = "block";
+    getTotal();
 });
-
 
 //price variables for shopping cart
 let darkRoastPrice = 11.99;
 let lightRoastPrice = 9.99;
 let celticGrogPrice = 11.49;
-let colombianDecafPrice = 12.49
+let colombianDecafPrice = 12.49;
 let ethiopianPrice = 10.99;
 let costaRicanPrice = 12.99;
 
@@ -71,7 +81,6 @@ const colombianDecafPriceTag = document.querySelector("#colombian-decaf-price");
 const ethiopianPriceTag = document.querySelector("#ethiopian-price");
 const costaRicanPriceTag = document.querySelector("#costa-rican-price");
 
-
 //set price variable in html
 darkRoastPriceTag.innerText = priceFormat(darkRoastPrice);
 lightRoastPriceTag.innerText = priceFormat(lightRoastPrice);
@@ -80,11 +89,10 @@ colombianDecafPriceTag.innerText = priceFormat(colombianDecafPrice);
 ethiopianPriceTag.innerText = priceFormat(ethiopianPrice);
 costaRicanPriceTag.innerText = priceFormat(costaRicanPrice);
 
-
 //formatting price
 function priceFormat(num) {
     let str = "";
-    return str = `$${num}/pound`;
+    return (str = `$${num}`);
 }
 
 let productValues = [];
@@ -93,25 +101,23 @@ let productValues = [];
 function addItem(id, value) {
     const ul = document.querySelector(".cart-items");
     const li = document.createElement("li");
-    li.innerText = `${id} - ${priceFormat(value)}`;
+    li.innerText = `${priceFormat(value)} - ${id}`;
     li.class = value;
     productValues.push(li.class);
     ul.appendChild(li);
 }
 
-function getTotal() {
-    let grandTotal = 0;
-    if (productValues.length === 0) {
-        document.querySelector("#default-cart-empty").style.display = "block";
-    } else {
-        for (let item of productValues) {
-            grandTotal += Number(item);
-        }
-        document.querySelector("#default-cart-empty").style.display = "none"; document.querySelector(".total").innerText = `Your total is ${grandTotal}`;
+function hideDefaultMessage() {
+    if (productValues.length > 0) {
+        document.querySelector("#default-cart-empty").style.display = "none";
     }
-
 }
 
-document.body.addEventListener("click", () => {
-    getTotal();
-});
+function getTotal() {
+    let subTotal = 0;
+    for (let item of productValues) {
+        subTotal += Number(item);
+    }
+    subTotal = Number.parseFloat(subTotal.toFixed(2));
+    document.querySelector(".total").innerText = `Your subtotal is $${subTotal}`;
+}
